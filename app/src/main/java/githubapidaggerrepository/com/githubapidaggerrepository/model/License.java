93977b9dfd0f39640a2877495a7a83c4749cc8c1
JosephAlemu <aaleemuu@gmail.com>
2018-05-10 18:40:05 -0400

    Pojo class created and dependencies libraries folders

---
+++ app/src/main/java/githubapidaggerrepository/com/githubapidaggerrepository/model/License.java
@@ -0,0 +1,95 @@
+
+package com.example;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.os.Parcelable.Creator;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class License implements Parcelable
+{
+
+    @SerializedName("key")
+    @Expose
+    private String key;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("spdx_id")
+    @Expose
+    private String spdxId;
+    @SerializedName("url")
+    @Expose
+    private String url;
+    public final static Parcelable.Creator<License> CREATOR = new Creator<License>() {
+
+
+        @SuppressWarnings({
+            "unchecked"
+        })
+        public License createFromParcel(Parcel in) {
+            return new License(in);
+        }
+
+        public License[] newArray(int size) {
+            return (new License[size]);
+        }
+
+    }
+    ;
+
+    protected License(Parcel in) {
+        this.key = ((String) in.readValue((String.class.getClassLoader())));
+        this.name = ((String) in.readValue((String.class.getClassLoader())));
+        this.spdxId = ((String) in.readValue((String.class.getClassLoader())));
+        this.url = ((String) in.readValue((String.class.getClassLoader())));
+    }
+
+    public License() {
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpdxId() {
+        return spdxId;
+    }
+
+    public void setSpdxId(String spdxId) {
+        this.spdxId = spdxId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(key);
+        dest.writeValue(name);
+        dest.writeValue(spdxId);
+        dest.writeValue(url);
+    }
+
+    public int describeContents() {
+        return  0;
+    }
+
+}
