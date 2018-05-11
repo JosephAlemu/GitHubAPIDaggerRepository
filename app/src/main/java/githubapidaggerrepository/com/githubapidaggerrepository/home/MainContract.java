b390ca2cdc3faec65f50e685cd591504b4ddba5f
JosephAlemu <aaleemuu@gmail.com>
2018-05-11 12:09:04 -0400

    created Maincontract interface to describe the interaction between View and Presenter

---
+++ app/src/main/java/githubapidaggerrepository/com/githubapidaggerrepository/home/MainContract.java
@@ -0,0 +1,26 @@
+package githubapidaggerrepository.com.githubapidaggerrepository.home;
+
+import java.util.List;
+import githubapidaggerrepository.com.githubapidaggerrepository.model.Repo;
+
+/**
+ * Created by user on 5/10/2018.
+ */
+public interface MainContract {
+
+
+    interface View {
+
+        void onsuccess(List<Repo> list);
+
+        void onfailure(String error);
+
+    }
+
+    interface Presenter {
+
+        void search(String title);
+
+    }
+
+}
